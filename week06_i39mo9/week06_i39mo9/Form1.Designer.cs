e6805da4347d82a77e5f6edb5e762d65a1bd392d
Márta Emese Orbán <marta.orban@stud.uni-corvinus.hu>
2021-10-25 22:11:40 +0200

    task06

--- week06_i39mo9/week06_i39mo9/Form1.Designer.cs
+++ week06_i39mo9/week06_i39mo9/Form1.Designer.cs
@@ -29,8 +29,13 @@ namespace week06_i39mo9
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.dgw = new System.Windows.Forms.DataGridView();
+            this.chartRateData = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.dgw)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRateData)).BeginInit();
             this.SuspendLayout();
             // 
             // dgw
@@ -38,18 +43,36 @@ namespace week06_i39mo9
             this.dgw.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgw.Location = new System.Drawing.Point(12, 12);
             this.dgw.Name = "dgw";
-            this.dgw.Size = new System.Drawing.Size(592, 368);
+            this.dgw.Size = new System.Drawing.Size(421, 368);
             this.dgw.TabIndex = 0;
+            // 
+            // chartRateData
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartRateData.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartRateData.Legends.Add(legend1);
+            this.chartRateData.Location = new System.Drawing.Point(439, 43);
+            this.chartRateData.Name = "chartRateData";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartRateData.Series.Add(series1);
+            this.chartRateData.Size = new System.Drawing.Size(385, 300);
+            this.chartRateData.TabIndex = 1;
+            this.chartRateData.Text = "chart1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(736, 392);
+            this.ClientSize = new System.Drawing.Size(827, 392);
+            this.Controls.Add(this.chartRateData);
             this.Controls.Add(this.dgw);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dgw)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRateData)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -57,6 +80,7 @@ namespace week06_i39mo9
         #endregion
 
         private System.Windows.Forms.DataGridView dgw;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartRateData;
     }
 }
 
--- week06_i39mo9/week06_i39mo9/Form1.cs
+++ week06_i39mo9/week06_i39mo9/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using week06_i39mo9.Entities;
 using week06_i39mo9.MNBServiceReference;
@@ -21,6 +22,7 @@ namespace week06_i39mo9
             InitializeComponent();
             dgw.DataSource = Rates;
             fuggveny2(fuggveny());
+            diagram();
         }
 
         private string fuggveny()
@@ -58,6 +60,25 @@ namespace week06_i39mo9
                 if (unit != 0)
                     rate.Value = value / unit;
             }
+        }
+
+        private void diagram()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
     }
 }
