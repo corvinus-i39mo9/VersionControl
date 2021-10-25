8faa735269141a5aba38b30ecd69d3a5b54a5d8c
Márta Emese Orbán <marta.orban@stud.uni-corvinus.hu>
2021-10-25 21:48:37 +0200

    task03

--- week06_i39mo9/week06_i39mo9/Form1.cs
+++ week06_i39mo9/week06_i39mo9/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week06_i39mo9.MNBServiceReference;
 
 namespace week06_i39mo9
 {
@@ -15,6 +16,21 @@ namespace week06_i39mo9
         public Form1()
         {
             InitializeComponent();
+            fuggveny();
+        }
+
+        private void fuggveny()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
