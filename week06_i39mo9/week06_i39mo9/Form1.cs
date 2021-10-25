3d465eb58946c2c390c6090594692dd6b2218e82
Márta Emese Orbán <marta.orban@stud.uni-corvinus.hu>
2021-10-25 21:53:25 +0200

    task04

--- week06_i39mo9/week06_i39mo9/Form1.cs
+++ week06_i39mo9/week06_i39mo9/Form1.cs
@@ -7,15 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week06_i39mo9.Entities;
 using week06_i39mo9.MNBServiceReference;
 
 namespace week06_i39mo9
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
+            dgw.DataSource = Rates;
             fuggveny();
         }
 
