f3d6effa3917eddd52bfe73df5aab03f88626a4d
Márta Emese Orbán <marta.orban@stud.uni-corvinus.hu>
2021-11-10 23:51:08 +0100

    car class hozzáadása

---
+++ i39mo9_week08_irf/i39mo9_week08_irf/Entities/Car.cs
@@ -0,0 +1,20 @@
+﻿using i39mo9_week08_irf.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace i39mo9_week08_irf.Entities
+{
+    public class Car: Toy
+    {
+        protected override void DrawImage(Graphics gr)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            gr.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
