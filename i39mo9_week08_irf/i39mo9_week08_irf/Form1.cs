ed3ad366f7624b8c3453585bd3c1b76b5eea2cea
Márta Emese Orbán <marta.orban@stud.uni-corvinus.hu>
2021-11-10 23:29:00 +0100

    futószalag létrehozása

--- i39mo9_week08_irf/i39mo9_week08_irf/Form1.cs
+++ i39mo9_week08_irf/i39mo9_week08_irf/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using i39mo9_week08_irf.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,44 @@ namespace i39mo9_week08_irf
 {
     public partial class Form1 : Form
     {
+        private List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var b in _balls)
+            {
+                b.MoveBall();
+                if (b.Left > maxPosition)
+                    maxPosition = b.Left;
+            }
+
+            if (maxPosition > 1000)
+            {
+                var maxBall = _balls[0];
+                mainPanel.Controls.Remove(maxBall);
+                _balls.Remove(maxBall);
+            }
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var b = Factory.CreateNew();
+            _balls.Add(b);
+            mainPanel.Controls.Add(b);
+            b.Left = -b.Width;
         }
     }
 }
